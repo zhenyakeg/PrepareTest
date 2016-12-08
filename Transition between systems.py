b09dbf306763dc603ef0b81e481261e6377e945d
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-12-08 18:48:56 +0300

    1

--- Transition between systems.py
+++ Transition between systems.py
@@ -1,21 +1,15 @@
 __author__ = 'student'
 
-import sys
-s = sys.stdin.read().split()
-
-n = int(s[0])
-A = int(s[1], base=n)
-k = int(s[2])
+x = int(input())
 def Transition(x , k):
-    Out = [str(i) for i in range(10)] + [chr(65+i) for i in range(26)]
+    Output = [str(i) for i in range(10)] + [chr(65+i) for i in range(26)]
     z = x
     s = ''
     if z == 0:
         return '0'
     while z != 0:
         i = z%k
-        s += Out[i]
+        s += Output[i]
         z//=k
     return s[::-1]
-
-print(Transition(A,k))
+print(Transition(x,17).count('G'))
