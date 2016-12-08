a24601453d3d12b04c3eeb84c5acc0a51ca1346b
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-12-08 18:50:58 +0300

    1

--- Pairs.py
+++ Pairs.py
@@ -1,11 +1,11 @@
 __author__ = 'student'
-Nums = []
-Input = list(map(int,input().split()))
-while Input != [0,0]:
-    Nums.append(Input)
-    Input = list(map(int,input().split()))
-j = 0
-for p in Nums:
+A = []
+In = list(map(int,input().split()))
+while In != [0,0]:
+    A.append(In)
+    In = list(map(int,input().split()))
+k = 0
+for p in A:
     if p[0] % 3 == 0 and p[1] % 2 == 0 or (p[0] % 9 == 0 and p[1] % 9 == 0):
-        j+=1
-print(j)
+        k+=1
+print(k)
